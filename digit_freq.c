8499b6bfe883fde0c06b93bd4ef8a80415f99613
Mamat <orsimchy100@gmail.com>
2024-11-26 18:39:16 +0200

    Q1 digit_freq.c update 26.11- Or

--- digit_freq.c
+++ digit_freq.c
@@ -4,8 +4,38 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main() {
+#define MAX_STRING_LEN 1000
 
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	char string[MAX_STRING_LEN];
+	fgets(string, MAX_STRING_LEN, stdin);
+	string[strlen(string) - 1] = '\0';
+
+	//create histogram for 10 digits
+	int hist[10] = {0};
+
+	//iterate over the string. for each char check if is number. if yes, update
+	//hist
+
+	for(int i=0; i< strlen(string); i++){
+		int value = *(string + i) - '0'; //reduce the ASCI value of 0- get int.
+		// after reducing 0 ASCI value - if the char was a number, now the
+		//value will be the number (int)
+
+		if(value >= 0 && value <= 9 ){
+			hist[value] ++;
+		}
+
+	}
+
+	//pront histogram
+	for(int i = 0; i< 9; i++){
+		printf("%d " , hist[i]);
+	}
+
+	printf("%d\n", hist[9]);
+
     return 0;
 }
